2ac51df511eec43bb1c6cdd8889cd3a57832cb1a
cplagge2 <cplagge@wisc.edu>
2023-02-17 19:53:04 -0600

    Added all files to command line

--- runParser.sh
+++ runParser.sh
@@ -1,5 +1,7 @@
-python ebay_parser.py ebay_data/items-0.json
-sort -u bids.dat > bids.dat
-sort -u item.dat > item.dat
-sort -u users.dat > users.dat
-sort -u categories.dat > categories.dat
+a=""
+for i in {0..39}
+do 
+a+="ebay_data/items-$i.json "
+done
+python ebay_parser.py $a
+sort -u users.dat > usersTest1.txt
