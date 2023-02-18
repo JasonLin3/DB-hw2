306b9f6899d8fde3800da9018b44b1e919341746
cplagge2 <cplagge@wisc.edu>
2023-02-17 18:36:28 -0600

    fixed errors in parsing and duplicate removal

--- runParser.sh
+++ runParser.sh
@@ -1,5 +1,5 @@
 python ebay_parser.py ebay_data/items-0.json
-$ sort -u bids.dat > bids.dat
-$ sort -u item.dat > item.dat
-$ sort -u users.dat > users.dat
-$ sort -u categories.dat > categories.dat
+sort -u bids.dat > bids.dat
+sort -u item.dat > item.dat
+sort -u users.dat > users.dat
+sort -u categories.dat > categories.dat
