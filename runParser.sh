9cac6f777950117f71bb390ad8435a6f62876620
cplagge2 <cplagge@wisc.edu>
2023-02-19 13:05:18 -0600

    fixed command prompt in shell

--- runParser.sh
+++ runParser.sh
@@ -1,7 +1,2 @@
-a=""
-for i in {0..39}
-do 
-a+="ebay_data/items-$i.json "
-done
-python ebay_parser.py $a
-sort -u users.dat > usersTest1.txt
+python ebay_parser.py ebay_data/items-*.json
+sort -u users.dat > usersTest3.txt
